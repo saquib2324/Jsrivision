d9906cc0e0ab5525b2de01294bae163b15b47552
saquibw2324 <mssaquib11@gmail.com>
2026-04-11 22:15:03 +0530

    this is rivision exersice

---
+++ first.js
@@ -0,0 +1,110 @@
+// console.log("saquib ansari");
+// let age=23;
+// const student ={
+
+// name: "saquib ansari",
+// age : 2,
+// cgpa:10,
+// isPass:true,
+
+// };
+
+// student.age=student.age+20;
+
+// console.log(student.age);
+// // conditional statements 
+
+// let umar=15;
+
+// if(umar>18){
+//     console.log("your age is greater than 18 ",umar,"yes you cann vote ");
+// }else{console.log("you can not vote");}
+
+
+// let number=prompt("enter the number ")
+
+// if(number%5===0){
+//     console.log("yes the number ",number,   "is divisible by 5 ");
+
+// }else{
+//     console.log("no the number",number,"is not divisible by 5");
+// }
+
+
+// let marks=Number(prompt("enter you marks:"));
+
+// if(marks>=90 && marks<=100){
+//     console.log("your grade is A ");
+// }else if(marks>=80 && marks<=89){
+//     console.log("your grade is b ");
+// }else if(marks>=70 && marks<=79){
+//     console.log("your grade is c");
+// }else  if(marks>=60 && marks<=69){
+//     console.log("your grade is D ");
+// }else if(marks>=50 && marks<=59){
+//     console.log("your grade is E ");
+
+// }else{
+//     console.log("padhai krle fail hai tu!!!! ");
+// }
+
+// for(let i=1;i<=5;i++){
+//     console.log("my name is saquib ansari 5 times ");
+
+// }
+// let size=5;
+// let count;
+
+// let sum=0;
+// for(let i=1;i<=size;i++){
+
+// sum=sum+i
+
+// }
+// console.log(sum);
+// let n="saqui ansari";
+
+// for(let i of n ){
+
+//     console.log("i=",i);
+// }
+
+ 
+// for(i=0;i<=100;i=i+2){
+//     console.log(i);
+// }
+
+
+// let gameNumber=25;
+
+// let guess=prompt("enter your number to guess:");
+
+
+// while(gameNumber!=guess){
+
+// guess=prompt("you entered wrong number , try again");
+// }
+// console.log(" Congradulation ! you have guess the right number ");
+
+
+let str="mssaqui";
+
+console.log(str);
+
+console.log(str.length);
+
+console.log(str[0]);
+
+
+let obj ={
+
+    item: "pen",
+    price:20,
+}
+
+let output=`the item ${obj.item} cost is ${obj.price} rupees`;
+
+console.log(output);
+
+
+
